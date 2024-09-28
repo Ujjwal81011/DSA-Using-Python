360e8b388d8ea75ef9db23d9cbc370d96a9217fe
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-09-28 22:43:48 +0530

    operators

---
+++ Basics/operators.py
@@ -0,0 +1,37 @@
+# Arithematic operator
+a = 15
+b = 14
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(b/2)
+# print(a%2)
+# print(a**2)
+
+# Assignment operator
+# a = a + 3
+# b = b - 4
+# print(a,b)
+
+# Comparision Operator
+# print( a==b )
+# print( a!=b )
+# print( a>b )
+# print( a<=b )
+
+# Logical Operator => and, or, not 
+x = 9
+y = 7
+print( x>5 and y<9)
+m = not( x>5 and y<9)
+print(m)
+
+# Membership Opertor => in, not in  with case sensetion
+h = "hello"
+print("e" in h)
+l = [10,20,30,40]
+print(50 not in l)
+
+# identity operator => is, is not
+print(x is y)
+print(x is not y)
