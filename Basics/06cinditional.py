a56ba77daac8071493d0100c11ad02c0d07f0014
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-09-29 23:22:49 +0530

    if elif else

---
+++ Basics/06cinditional.py
@@ -0,0 +1,8 @@
+a = int(input("enter the value of a"))
+b = int(input("enter the value of b"))
+if a>b:
+    print("number is greater than b")
+elif a==b:
+    print("number is equals to b")
+else:
+    print("number is less than b")
