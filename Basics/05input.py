22a2bdd92902393874e082ea05d7a718085477e8
Ujjwal Mishra <mishraujjwal9936@gmail.com>
2024-12-08 19:36:08 +0530

    Input

--- Basics/05input.py
+++ Basics/05input.py
@@ -1,6 +1,6 @@
 a = int(input("enter the value of a in integer"))
 b = int(input("enter the value of b in integer"))
 sum = a+b
-print("the value of sum is ",sum ) 
+print("The value of sum is ",sum ) 
 mul = a*b
 print("The value of mul is", mul)
